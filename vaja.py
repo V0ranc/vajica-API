7da579280529a458c07812a24decc46a683eff67
V0ranc <voranc.rekar@sts.sckr.si>
2026-02-24 10:33:47 +0000

    flask,vaja3

--- vaja.py
+++ vaja.py
@@ -1,41 +1,40 @@
-import requests
+from flask import Flask, render_template, request
+import random
+app = Flask(__name__)
 
-url = "https://restcountries.com/v3.1/all?fields=name"
-odgovor = requests.get(url)
-drzave = odgovor.json()
-dr = []
-for d in drzave[:5]:
-    print(d)
-#print(drzave)
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-# 1: Poišči državo z največ sosedi (borders)
-# Namig: Nekatere države so otoki in nimajo ključa "borders"!
-#for d in drzave:
- #   dr.append(d.get("name").get("official"))
-#print(dr)
+@app.route("/coinflip")
+def coinflip():
+    return render_template("coinflip.html")
 
-# 2: Poišči države kjer govorijo največ jezikov (languages)
-# Namig: Nekatere države nimajo ključa "languages"
-
+@app.route("/coinData")
+def coinData():
+    print(request.args["vrednost"])
+    rnd = random.randint(0, 1)
+    status = ["HEADS", "TAILS"][rnd]
+    if rnd == 0:
+        return {"img": "https://i.postimg.cc/CBNJNfDJ/head.png", "status": status}
+    else:
+        return {"img": "https://i.postimg.cc/zysdXN8w/tail.png", "status": status}
     
+@app.route("/randomQuote")
+def randomQuote():
+    quotes = [
+    "The only way to do great work is to love what you do. – Steve Jobs",
+    "Life is what happens when you're busy making other plans. – John Lennon",
+    "In the end, we will remember not the words of our enemies, but the silence of our friends. – Martin Luther King Jr.",
+    "The purpose of life is not to be happy. It is to be useful, to be honorable, to be compassionate, to have it make some difference that you have lived and lived well. – Ralph Waldo Emerson",
+    "Be yourself; everyone else is already taken. – Oscar Wilde",
+    "You miss 100% of the shots you don’t take. – Wayne Gretzky",
+    "It is never too late to be what you might have been. – George Eliot",
+    "Success is not the key to happiness. Happiness is the key to success. – Albert Schweitzer",
+    "Do not go where the path may lead, go instead where there is no path and leave a trail. – Ralph Waldo Emerson",
+    "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment. – Ralph Waldo Emerson"
+    ]
+    quote = random.choice(quotes)
+    return {"img": "url", "status": quote}
 
-# 3: Izračunaj povprečno število prebivalcev (population) po celinah (continents)
-# Namig: Vedno preveri, če je population večji od 0
-
-# 4: Poišči državo z največ časovnimi pasovi (timezones)
-# Namig: Vsaka država ima vsaj en timezone
-
-# 5: Izpiši vse države, ki imajo v imenu presledek
-# Namig: Uporabi ["name"]["common"] za ime države
-
-# 6: Izpiši število držav, ki imajo za uradni jezik angleščino
-
-# 7: V katerem časovnem pasu (timezone) je največ držav?
-# Namig: Ena država ima lahko več timezone-ov
-
-# 8: Katera črka se največkrat pojavi kot prva črka v imenu države?
-# Namig: Za ime uporabi ["name"]["common"].lower()
-
-# 9: Katera država ima najdaljše ime?
-
-# 10: Izračunaj še eno statistiko po želji.
+app.run(debug=True)
